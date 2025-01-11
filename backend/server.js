7611231d977a26ab8e56a96e0b3bff1526a66adb
computerdev21 <devchetal@gmail.com>
2025-01-11 07:07:02 -0500

    Server updates

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import chatRoute from './routes/chat.js';
 
 dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Backend is live 🚀'));
+app.use('/api/chat', chatRoute);
 
-app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+app.listen(5000, () => {
+    console.log('🔊 Backend running at http://localhost:5000');
+});
