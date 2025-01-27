586e907fd55c28c8469882bf3e0b6c6cb6b0c906
computerdev21 <devchetal@gmail.com>
2025-01-27 03:01:13 -0500

    Backend Working

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const cors = require('cors');
 const chatRoute = require('./routes/chat');
 const uploadRoute = require('./routes/upload');
+const analyzeRoute = require('./routes/analyze');
 
 const app = express();
 app.use(cors());
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use('/api/chat', chatRoute);
 app.use('/api/upload', uploadRoute);
+app.use('/api/analyze', analyzeRoute);
 
 app.listen(5001, () => {
     console.log('🔊 Backend running at http://localhost:5001');
